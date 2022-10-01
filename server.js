d0f8396bd611b8aa2ecc468b30f51d23c7d133cd
Ashley Gutierrez <ashgracie1994@icloud.com>
2022-10-01 14:07:07 -0700

    Homework Completed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 let express = require('express');
+const res = require('express/lib/response');
 let app = express();
 
 let server = app.listen(8081, function () {
@@ -12,6 +13,19 @@ app.get('/greeting/:name', function (req, res) {
     res.send(`Hello there, ${req.params.name}`);
  })
 
+app.get('/tip/:total/:tipPercentage',(req,res)=>{
+    let total = req.params.total
+    let percent = req.params.tipPercentage
+    let answer = total * percent / 100
+    res.send(`A ${percent}% on $${total} is $${answer}`)
+   })
 
 
- 
+  let answers = ["It is certain", "It is decidedly so", "Without a doubt", "Yes definitely","You may rely on it", "As I see it yes", "Most likely", "Outlook good","Yes", "Signs point to yes", "Reply hazy try again", "Ask again later","Better not tell you now", "Cannot predict now", "Concentrate and ask again","Don't count on it", "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
+app.get('/Magic/:question', (req, res) => {
+    let answer = answers[Math.floor(Math.random()*answers.length)]
+    res.send(`${req.params.question}?<hr/><h1>${answer}</h1>`)
+})
+
+
+
